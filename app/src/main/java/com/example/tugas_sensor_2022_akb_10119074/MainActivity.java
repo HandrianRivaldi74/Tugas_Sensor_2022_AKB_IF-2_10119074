4065e31267c06429c0542ab8dc0a86fb18909298
HandrianRivaldi74 <handrianr28@gmail.com>
2022-07-01 23:00:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas_sensor_2022_akb_10119074/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tugas_sensor_2022_akb_10119074;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Nim : 10119074
+//Nama : Handrian R
+//Kelas : IF2
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
